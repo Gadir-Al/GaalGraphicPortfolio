705c2d26fbabca4dd8e5b8cc7ae3e995a2763de4
Gadir-Al <99508056+Gadir-Al@users.noreply.github.com>
2023-10-26 20:55:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-  sections
