ef40def487e84a8da7d3585f4de0869c8494b288
Gadir-Al <soulsilver054@gmail.com>
2023-10-29 01:23:11 -0400

    initial commit

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
+
+function myFunction(){
+    var x = document.getElementById("myNavigation");
+    if(x.className === "navigation"){
+        x.className += " responsive ";   
+    } else {
+        x.className = "navigation";
+    }
+}
